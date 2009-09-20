d2aad4e55f6429051b402d21f3535bbd39a3b376
David Underhill <dgu@cs.stanford.edu>
2009-09-20 14:49:37 -0400

    add debug logging to TopologyResolver

--- TopologyResolver.py
+++ TopologyResolver.py
@@ -44,24 +44,30 @@ class TopologyResolver:
         for mac in topo.get_my_mac_addrs():
             try:
                 self.m2t[mac].append(topo)
+                logging.debug('%s: MAC registered: %s (shared)' % (topo, addrstr(mac)))
             except KeyError:
                 self.m2t[mac] = [topo]
+                logging.debug('%s: MAC registered: %s' % (topo, addrstr(mac)))
 
         for ip in topo.get_my_ip_addrs():
             # get the source filtering tree associated with the destination
             try:
                 st = self.i2t[ip]
+                logging.debug('%s: IP registered: %s (shared)' % (topo, addrstr(ip)))
             except KeyError:
                 st = IterableSubnetTree()
                 self.i2t[ip] = st
+                logging.debug('%s: IP registered: %s' % (topo, addrstr(ip)))
 
             # register which sources this topo wants packets from
             for ps in topo.get_source_filters():
                 if st.has_exact(ps):
                     # another topo has both this dst IP and the same source filter
                     st[ps].append(topo)
+                    logging.debug('%s: Source filter: %s (shared)' % (topo, ps))
                 else:
                     st[ps] = [topo]
+                    logging.debug('%s: Source filter: %s' % (topo, ps))
 
     def unregister_topology(self, topo):
         """Unregisters a topology with the resolver."""
@@ -117,23 +123,34 @@ class TopologyResolver:
         try:
             st = self.i2t[dst_ip]
         except KeyError:
+            # logging.debug('Ignoring packet to %s' % addrstr(dst_ip))
             return [] # no topology has this IP address
 
         # If there is no source IP, return all of the topologies which have this
         # destination address.
         if not src_ip:
+            logging.debug('No source IP given.  All topos mapped by %s: %s' % \
+                          (addrstr(dst_ip), ','.join([str(t.id) for t in st.values()])))
             return st.values()
 
         # Figure out which topology is interested in packets from this source.
         try:
-            return st[src_ip]
+            topos = st[src_ip]
+            logging.debug('IP pair resolved to topo: (%s->%s) -> %s' % \
+                          (addrstr(src_ip), addrstr(dst_ip), ','.join([str(t.id) for t in topos])))
+            return topos
         except KeyError:
+            logging.debug('IP pair ignored (not interested in the source): %s->%s' % \
+                          (addrstr(src_ip), addrstr(dst_ip)))
             return [] # nobody is interested in packets from this source
 
     def resolve_mac(self, dst_mac):
         """Resolves a dst MAC address to a list of topologies to which the
         packet to this dst should be forwarded."""
         try:
-            return self.m2t[dst_mac]
+            topos = self.m2t[dst_mac]
+            logging.debug('MAC %s resolved to topo %d' % (addrstr(dst_mac), ','.join([str(t.id) for t in topos])))
+            return topos
         except KeyError:
+            # logging.debug('Ignoring packet to MAC %s' % addrstr(dst_mac))
             return [] # no topology has this MAC address
