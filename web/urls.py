f07129c4a83c7ed04b41d2b75c8cf4f4e04cc2f6
David Underhill <dgu@cs.stanford.edu>
2009-10-03 19:34:01 -0400

    added summary page which lists active clients   -- presentation is very minimal right now

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,20 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.generic import list_detail
+from django.views.generic.simple import direct_to_template
+
+from vns import models as db
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+summary_info = {
+    'queryset': db.StatsTopology.objects.filter(active=True).order_by('id'),
+    'template_name': 'vns/current_usage.html',
+    'template_object_name': 'stats'
+}
+
+urlpatterns = patterns('vns.views',
     (r'^admin/', include(admin.site.urls)),
+    (r'^summary/?$', list_detail.object_list, summary_info),
+    (r'^vns.css$', direct_to_template, {'mimetype':'text/css', 'template':'vns.css'}),
 )
