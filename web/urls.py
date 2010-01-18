2519113405af9beb4bfb6ab267c38d646ad83900
David Underhill <dgu@cs.stanford.edu>
2010-01-17 19:03:47 -0500

    Add a page which shows allocated topologies and links to their readmes.

--- web/urls.py
+++ web/urls.py
@@ -13,9 +13,16 @@ summary_info = {
     'template_object_name': 'stats'
 }
 
+topologies_info = {
+    'queryset': db.Topology.objects.filter(enabled=True).order_by('owner__userprofile__org__name', 'template__name', 'id'),
+    'template_name': 'vns/topologies.html',
+    'template_object_name': 'topos'
+}
+
 urlpatterns = patterns('web.vnswww.views',
     (r'^admin/', include(admin.site.urls)),
     (r'^summary/?$', list_detail.object_list, summary_info),
+    (r'^topologies/?$', list_detail.object_list, topologies_info),
     (r'^topology=(?P<tid>\d+)$', 'topology_to_xml'),
     (r'^topology_readme=(?P<tid>\d+)$', 'topology_readme'),
     (r'^vns.css$', direct_to_template, {'mimetype':'text/css', 'template':'vns.css'}),
