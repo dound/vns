8c615606146286298b7278ae441ddded5726e081
David Underhill <dgu@cs.stanford.edu>
2010-04-11 12:44:49 -0400

    refactor: store tid, username, and auth_key with the conn object

--- topo_interactor.py
+++ topo_interactor.py
@@ -127,24 +127,24 @@ Connects to the VNS Topology Interaction service."""
 
     # connect to the server and handle messages it sends us
     print 'Connecting to VNS server at %s ...' % options.server
-    mr = lambda c,m : msg_received(c,m,options.topo_id, options.username, auth_key)
-    client = LTTwistedClient(TI_PROTOCOL, mr, got_connected, got_disconnected, False)
+    gc = lambda c : got_connected(c,options.topo_id, options.username, auth_key)
+    client = LTTwistedClient(TI_PROTOCOL, msg_received, gc, got_disconnected, False)
     client.connect(options.server, TI_DEFAULT_PORT)
     reactor.run()
 
-def msg_received(conn, msg, tid, username, auth_key):
+def msg_received(conn, msg):
     """Handles messages received from the TI server.  Starts the
     TopologyInteractor command-line interface once authentication is complete."""
     if msg is not None:
         if msg.get_type() == VNSAuthRequest.get_type():
-            print 'Authenticating as %s' % username
-            sha1_of_salted_pw = hashlib.sha1(msg.salt + auth_key).digest()
-            conn.send(VNSAuthReply(username, sha1_of_salted_pw))
+            print 'Authenticating as %s' % conn.username
+            sha1_of_salted_pw = hashlib.sha1(msg.salt + conn.auth_key).digest()
+            conn.send(VNSAuthReply(conn.username, sha1_of_salted_pw))
         elif msg.get_type() == VNSAuthStatus.get_type():
             print 'got auth status'
             if msg.auth_ok:
                 print 'Authentication successful.'
-                conn.send(TIOpen(tid))
+                conn.send(TIOpen(conn.tid))
                 reactor.callInThread(TopologyInteractor(conn).cmdloop)
             else:
                 print 'Authentication failed.'
@@ -155,8 +155,11 @@ def msg_received(conn, msg, tid, username, auth_key):
         else:
             print 'unexpected TI message received: %s' % msg
 
-def got_connected(conn):
+def got_connected(conn, tid, username, auth_key):
     print 'Connected!'
+    conn.tid = tid
+    conn.username = username
+    conn.auth_key = auth_key
 
 def got_disconnected(conn):
     print 'Disconnected!'
